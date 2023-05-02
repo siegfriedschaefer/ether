b6484286f3ad3d29918391f5e7a7bad664adb891
Siegfried Schaefer <siegfried.schaefer@gmail.com>
2023-05-02 20:36:44 +0200

    reconfigure package as monorepo

--- packages/minimal-startup/src/index.ts
+++ packages/minimal-startup/src/index.ts
@@ -1,7 +1,10 @@
 import { ethers } from 'ethers';
 
+const ARBITRUM_ENDPOINT = 'https://arb1.arbitrum.io/rpc';
+const KAVA_ENDPOINT = 'https://evm.kava.io';
+
 const main = async () => {
-    const provider = new ethers.JsonRpcProvider('https://arb1.arbitrum.io/rpc');
+    const provider = new ethers.JsonRpcProvider(KAVA_ENDPOINT);
     const blockNumber = await provider.getBlockNumber();
     console.log('blockNumber: ', blockNumber);
 }
